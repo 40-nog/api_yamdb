3ec1b1f58023a060234569f2b7c5b14414ebf352
Наталья Сорокина <natalya_40ina@mail.ru>
2022-10-21 21:08:33 +0300

    поработала с приложением reviews

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+from django.core.validators import MinValueValidator, MaxValueValidator
+from users.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=250)
+    year = models.IntegerField()
+    rating = models.IntegerField()
+    description = models.TextField(blank=True)
+    genre = models.ManyToManyField(Genre)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+
+
+class Review(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    score = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    pub_date = models.DateTimeField(auto_now_add=True)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE, related_name='reviews')
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
